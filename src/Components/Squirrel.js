f30697ec756c0f964ed50385885c5aebfab90694
ialonso3 <ialonso@manhattanbridgeshs.org>
2021-03-13 16:18:51 -0400

    Update Squirrel.js

--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -4,6 +4,13 @@ function Squirrel(props) {
   return (
     <div className="Squirrel">
       <h3>SQUIRREL CARD!</h3>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age==="Adult" ? "🧓" : "🍼"}</p>
+      <p>Color: {props.color==="Gray" ? "Gray" : "Not Gray"}</p>
+      <p>{props.running ? "🏃" : ""}</p>
+      <p>{props.eating ? "🍴" : ""}</p>
+      <p>{props.approaches ? "Approaching" : ""}</p>
+      <p>{props.climbing ? "🧗" : ""} </p>
     </div>
   );
 }
